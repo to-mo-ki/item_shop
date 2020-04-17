cfbf865f2b612f6b081df2fa408b5c5817ee2f47
tomoki <ntkmnk1995756@gmail.com>
2020-04-17 17:09:30 +0900

    networkによるsecondsPerBlockの切り替え

--- migrations/1584179526_item_shop.js
+++ migrations/1584179526_item_shop.js
@@ -1,9 +1,16 @@
 var ItemShop = artifacts.require("ItemShop");
 var ItemToken = artifacts.require("ItemToken");
 
-module.exports = async (deployer) => {
+module.exports = async (deployer, network) => {
+  if (network == "development") {
+    secondsPerBlock = 1;
+  } else if (network == "rinkeby") {
+    secondsPerBlock = 15;
+  } else {
+    secondsPerBlock = 1;
+  }
   await deployer.deploy(ItemToken);
-  await deployer.deploy(ItemShop, ItemToken.address, 1);
+  await deployer.deploy(ItemShop, ItemToken.address, secondsPerBlock);
   itemTokenInstance = await ItemToken.deployed();
   itemTokenInstance.transferOwnership(ItemShop.address);
 };
