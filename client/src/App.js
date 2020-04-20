e10c8f5ec4318f8b32e06680565cd67d8aaf65ff
tomoki <ntkmnk1995756@gmail.com>
2020-04-20 16:44:47 +0900

    ネットワークタイプのチェック

--- client/src/App.js
+++ client/src/App.js
@@ -6,8 +6,13 @@ import './App.css';
 import Main from './Main';
 import Header from './Header';
 
-const App = initialized => {
+const App = (initialized, drizzle) => {
   if (!initialized) return 'Loading...';
+  drizzle.web3.eth.net.getNetworkType((err, networkType) => {
+    if (networkType !== process.env.REACT_APP_NETWORK_TYPE) {
+      alert("ネットワークを" + process.env.REACT_APP_NETWORK_TYPE + "に切り替えてください");
+    }
+  });
   return (
     <Container>
       <Header />
@@ -18,7 +23,7 @@ const App = initialized => {
 
 const withContext = () => (
   <DrizzleContext.Consumer>
-    {({ initialized }) => App(initialized)}
+    {({ initialized, drizzle }) => App(initialized, drizzle)}
   </DrizzleContext.Consumer>
 );
 export default withContext;
