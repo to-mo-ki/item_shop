4efda22497c0484d5464d087bd70ff5ceb6ece4d
tomoki <ntkmnk1995756@gmail.com>
2020-04-20 12:38:39 +0900

    widtrawPaymentsのクライアント側実装

--- client/src/AdminPage.js
+++ client/src/AdminPage.js
@@ -1,11 +1,13 @@
 import React from "react";
 import AddItem from './AddItem';
+import WithdrawButton from './WithdrawButton';
 
 export default class Home extends React.Component {
   render() {
     return (
       <div>
         <AddItem />
+        <WithdrawButton />
       </div>
     );
   }
---
+++ client/src/WithdrawButton.js
@@ -0,0 +1,51 @@
+import React, { Component } from 'react';
+import { DrizzleContext } from '@drizzle/react-plugin';
+import Button from 'react-bootstrap/Button';
+
+class WithdrawButton extends Component {
+  state = { stackId: null };
+
+  constructor(props) {
+    super(props);
+    this.onClick = () => {
+      const { drizzle, drizzleState } = this.props;
+      const contract = drizzle.contracts.ItemShop;
+      const stackId = contract.methods.withdrawPayments.cacheSend({
+        from: drizzleState.accounts[0],
+      });
+      this.setState({ stackId });
+      //this.props.turnFetchStatus(true);
+    };
+  }
+
+  getTxStatus = () => {
+    // get the transaction states from the drizzle state
+    const { transactions, transactionStack } = this.props.drizzleState;
+    // get the transaction hash using our saved `stackId`
+    const txHash = transactionStack[this.state.stackId];
+    // if transaction hash does not exist, don't display anything
+    if (!txHash) return null;
+    // otherwise, return the transaction status
+    return `Transaction status: ${transactions[txHash] && transactions[txHash].status}`;
+  };
+
+  render() {
+    var button = <Button variant="primary" onClick={() => this.onClick()}>Withdraw</Button>;
+    return (<div>
+      {button}
+      {this.getTxStatus()}
+    </div>);
+  }
+}
+
+const withContext = props => (
+  <DrizzleContext.Consumer>
+    {({ drizzle, drizzleState }) => (
+      <WithdrawButton
+        drizzle={drizzle}
+        drizzleState={drizzleState}
+      />
+    )}
+  </DrizzleContext.Consumer>
+);
+export default withContext;
