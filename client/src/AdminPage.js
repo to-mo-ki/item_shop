75f9d7ecc1ecd4dfe5ee04ce26a4a8d39307298a
tomoki <ntkmnk1995756@gmail.com>
2020-04-20 11:57:26 +0900

    管理者ページの作成

--- client/src/AdminPage.js
+++ client/src/AdminPage.js
@@ -1,11 +1,11 @@
 import React from "react";
-import ShowItemShop from './ShowItemShop';
+import AddItem from './AddItem';
 
 export default class Home extends React.Component {
   render() {
     return (
       <div>
-        <ShowItemShop />
+        <AddItem />
       </div>
     );
   }
