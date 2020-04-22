08a359afe57de9145acf373751937fa5412d090e
tomoki <ntkmnk1995756@gmail.com>
2020-04-22 18:45:48 +0900

    本番環境とテスト環境でのipfsのURLの使い分け

--- client/src/ipfs.js
+++ client/src/ipfs.js
@@ -1,4 +1,8 @@
 const ipfsClient = require('ipfs-http-client');
-const ipfs = ipfsClient({ host: '127.0.0.1', port: 5001 });
+const ipfs = ipfsClient({
+  host: process.env.REACT_APP_IPFS_HOST,
+  port: 5001,
+  protocol: process.env.REACT_APP_IPFS_PROTOCOL
+});
 
 export default ipfs;
