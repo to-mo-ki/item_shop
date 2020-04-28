df069791fd58b6cce9bb07d4d30423e8aa35269d
axm-nakano <nakano@axelmark.co.jp>
2020-04-28 16:48:48 +0900

    ShowMyItemの削除

--- client/src/MyItem.js
+++ client/src/MyItem.js
@@ -1,14 +1,14 @@
-import React from "react";
-import ShowMyItem from "./ShowMyItem"
-import ExhibitForm from "./ExhibitForm"
+import React from 'react'
+import MyItemList from './MyItemList'
+import ExhibitForm from './ExhibitForm'
 
-export default class MyItem extends React.Component {
-  render() {
-    return (
-      <div>
-        <ExhibitForm />
-        <ShowMyItem />
-      </div>
-    );
-  }
+function MyItem () {
+  return (
+    <div>
+      <ExhibitForm />
+      <MyItemList />
+    </div>
+  )
 }
+
+export default MyItem
--- client/src/MyItemList.js
+++ client/src/MyItemList.js
@@ -1,24 +1,54 @@
-import React, { Component } from 'react'
+import React, { useState, useEffect } from 'react'
+import { DrizzleContext } from '@drizzle/react-plugin'
 import CardColumns from 'react-bootstrap/CardColumns'
 import ItemCard from './ItemCard'
 
-class MyItemList extends Component {
-  render () {
-    const account = this.props.account
-    const rows = this.props.itemToOwner.map(function (owner, index) {
-      if (account !== owner) {
-        return null
-      }
-      console.log(index)
-      return <ItemCard key={index} id={index}/>
-    })
+function MyItemList (props) {
+  const [keys, setKeys] = useState([])
 
-    return (
-      <CardColumns>
-        {rows}
-      </CardColumns>
-    )
+  const fetchItemKeys = async (contract) => {
+    const itemCount = await contract.methods.getItemCount().call()
+    const keys = []
+    for (let i = 0; i < itemCount; i++) {
+      keys.push(contract.methods.ownerOf.cacheCall(i))
+    }
+    setKeys(keys)
   }
+
+  useEffect(() => {
+    fetchItemKeys(props.drizzle.contracts.ItemShop)
+  }, [props.drizzle])
+
+  const { ItemShop } = props.drizzleState.contracts
+  const itemToOwner = keys.map(key =>
+    ItemShop.ownerOf[key] ? ItemShop.ownerOf[key].value : undefined
+  )
+
+  const account = props.drizzleState.accounts[0]
+  const rows = itemToOwner.map(function (owner, index) {
+    if (account !== owner) {
+      return null
+    }
+    console.log(index)
+    return <ItemCard key={index} id={index}/>
+  })
+
+  return (
+    <CardColumns>
+      {rows}
+    </CardColumns>
+  )
 }
 
-export default MyItemList
+const withContext = () => (
+  <DrizzleContext.Consumer>
+    {({ drizzle, drizzleState }) => (
+      <MyItemList
+        drizzle={drizzle}
+        drizzleState={drizzleState}
+      />
+    )}
+  </DrizzleContext.Consumer>
+)
+
+export default withContext
