2a923a6b8fad6cc57527e8db1b689f5190c4b104
axm-nakano <nakano@axelmark.co.jp>
2020-04-28 14:24:02 +0900

    ItemMetaURIの作成

--- client/src/MyItemList.js
+++ client/src/MyItemList.js
@@ -2,17 +2,17 @@ import React, { Component } from 'react'
 import { DrizzleContext } from '@drizzle/react-plugin'
 import CardColumns from 'react-bootstrap/CardColumns'
 import ItemCard from './ItemCard'
+import ItemMetaURI from './ItemMetaURI'
 
 class MyItemList extends Component {
   render () {
     const account = this.props.account
-    const URIs = this.props.URIs
     const rows = this.props.itemToOwner.map(function (owner, index) {
       if (account !== owner) {
         return null
       }
       console.log(index)
-      return <ItemCard key={index} id={index} URI={URIs[index]} />
+      return <ItemMetaURI key={index} id={index}/>
     })
 
     return (
