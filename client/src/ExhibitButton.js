da6aa81a5368155b98749d672a812d8fac2ed3d6
axm-nakano <nakano@axelmark.co.jp>
2020-05-08 10:15:30 +0900

    ExhibitButtonのReact hooks化

--- client/src/ExhibitButton.js
+++ client/src/ExhibitButton.js
@@ -1,42 +1,37 @@
-import React, { Component } from 'react';
-import Button from 'react-bootstrap/Button';
-import withDrizzleContext from './withDrizzleContext';
+import React, { useState } from 'react'
+import Button from 'react-bootstrap/Button'
+import withDrizzleContext from './withDrizzleContext'
 
 const style = {
-  margin:"10px"
+  margin: '10px'
 }
-class ExhibitButton extends Component {
-  state = { stackId: null };
+function ExhibitButton (props) {
+  const [stackId, setStackId] = useState('')
 
-  constructor(props) {
-    super(props);
-    this.onClick = (index, startPrice, endPrice, duration) => {
-      const { drizzle, drizzleState } = this.props;
-      const contract = drizzle.contracts.ItemShop;
-      const startWeiPrice = drizzle.web3.utils.toWei(startPrice, "ether");
-      const endWeiPrice = drizzle.web3.utils.toWei(endPrice, "ether");
-      const stackId = contract.methods.exhibit.cacheSend(index, startWeiPrice, endWeiPrice, duration, {
-        from: drizzleState.accounts[0],
-      });
-      this.setState({ stackId });
-    };
+  const onClick = (index, startPrice, endPrice, duration) => {
+    const { drizzle, drizzleState } = this.props
+    const contract = drizzle.contracts.ItemShop
+    const startWeiPrice = drizzle.web3.utils.toWei(startPrice, 'ether')
+    const endWeiPrice = drizzle.web3.utils.toWei(endPrice, 'ether')
+    const stackId = contract.methods.exhibit.cacheSend(index, startWeiPrice, endWeiPrice, duration, {
+      from: drizzleState.accounts[0]
+    })
+    setStackId(stackId)
   }
 
-  getTxStatus = () => {
-    const { transactions, transactionStack } = this.props.drizzleState;
-    const txHash = transactionStack[this.state.stackId];
-    if (!txHash) return null;
-    return `Transaction status: ${transactions[txHash] && transactions[txHash].status}`;
-  };
-
-  render() {
-    var { index, startPrice, endPrice, duration } = this.props;
-    var button = <Button variant="primary" style={style} onClick={() => this.onClick(index, startPrice, endPrice, duration)}>Exihibit</Button>;
-    return (<div>
-      {button}
-      {this.getTxStatus()}
-    </div>);
+  const getTxStatus = () => {
+    const { transactions, transactionStack } = props.drizzleState
+    const txHash = transactionStack[stackId]
+    if (!txHash) return null
+    return `Transaction status: ${transactions[txHash] && transactions[txHash].status}`
   }
+
+  var { index, startPrice, endPrice, duration } = props
+  var button = <Button variant="primary" style={style} onClick={() => onClick(index, startPrice, endPrice, duration)}>Exihibit</Button>
+  return (<div>
+    {button}
+    {getTxStatus()}
+  </div>)
 }
 
 export default withDrizzleContext(ExhibitButton)
