c1064ff5516561981188bc68d530cfa7a6a7d526
axm-nakano <nakano@axelmark.co.jp>
2020-05-08 12:50:39 +0900

    管理者ページで管理者以外がアクセスすると警告を出すようにした

--- client/src/admin/AdminPage.js
+++ client/src/admin/AdminPage.js
@@ -1,10 +1,40 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import AddItem from './AddItem'
+import withDrizzleContext from '../common/withDrizzleContext'
 
-export default function AdminPage () {
+function AdminPage (props) {
+  const [owner, setOwner] = useState('')
+  const [key, setKey] = useState('')
+  const [account, setAccount] = useState(undefined)
+
+  useEffect(() => {
+    const contract = props.drizzle.contracts.ItemShop
+    const key = contract.methods.owner.cacheCall()
+    setKey(key)
+  }, [props.drizzle])
+
+  useEffect(() => {
+    const contract = props.drizzleState.contracts.ItemShop
+    const owner = contract.owner[key] ? contract.owner[key].value : undefined
+    setOwner(owner)
+  }, [key, props.drizzleState])
+
+  useEffect(() => {
+    setAccount(props.drizzleState.accounts[0])
+  }, [props.drizzleState])
+
+  useEffect(() => {
+    if ((!owner) || (!account)) return
+    if (owner !== account) alert('管理者ページは管理者しかアクセスできません')
+  }, [account, owner])
+
+  if (owner !== account) return null
+
   return (
     <div>
       <AddItem />
     </div>
   )
 }
+
+export default withDrizzleContext(AdminPage)
---
+++ client/src/useContractAdd.js
@@ -0,0 +1,5 @@
+import { useState } from 'react'
+
+function useContractAdd (drizzleState, drizzle) {
+  const [state, setState] = useState('initial')
+}
